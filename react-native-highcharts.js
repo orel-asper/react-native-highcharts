d1d8abe5304db33bc141c64d8c1e5946a4acd2ae
orel asper <67417080+orel-asper@users.noreply.github.com>
2024-08-19 17:41:04 +0300

    Update react-native-highcharts.js

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -1,137 +1,53 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import { WebView } from 'react-native-webview';
-
+import exportingScript from './exporting';
+import  highstockScript from './highstock'
 const win = Dimensions.get('window');
 
 class ChartWeb extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            init: `
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <script src="https://code.highcharts.com/stock/highstock.js"></script>
-                        <script src="https://code.highcharts.com/modules/exporting.js"></script>
-                        <style>
-                            @font-face {
-                                font-family: 'OpenSansHebrew-Regular';
-                                src: url('fonts/OpenSansHebrew-Regular.ttf') format('truetype');
-                                font-weight: 300;
-                            }
-                            #container {
-                                width: 100%;
-                                height: 100%;
-                                position: absolute;
-                                top: 0;
-                                left: 0;
-                                font-weight: 300;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div id="container"></div>
-                    </body>
-                </html>
-            `,
-            end: `
-                <script>
-                    var config = ${JSON.stringify(this.props.config)};
-                    config.chart = {
-                        ...config.chart,
-                        style: {
-                            fontSize: '11px',
-                            fontWeight: '300'
-                        }
-                    };
-                    config.tooltip = {
-                        ...config.tooltip,
-                        style: {
-                            fontSize: '11px',
-                            fontWeight: '300'
-                        },
-                        formatter: function() {
-                            var selectedTimeFilter = config.tooltip.selectedTimeFilter;
-                            var strings = config.tooltip.strings;
-                            var isMultiChartPresentation = config.tooltip.isMultiChartPresentation;
-                            var tradeDataForStock = config.tooltip.tradeDataForStock;
-                            var registrationDate = config.tooltip.registrationDate;
-
-                            var dateFormat = selectedTimeFilter === 0 ? '%H:%M:%S' : '%d/%m/%y';
-                            var date = Highcharts.dateFormat(dateFormat, this.x);
-                            var tradeDataForStockString = selectedTimeFilter === 0 ? strings.rate : registrationDate === date ? strings.registrationRateStrings : strings.closingRate;
-                            var title = isMultiChartPresentation ? strings.yield : (tradeDataForStock ? tradeDataForStockString : strings.index);
-
-                            var value = isMultiChartPresentation ? this.y.toFixed(2) + '%' : this.y.toFixed(2);
-                            if (value.indexOf('-') > -1) {
-                                value = value.substring(1) + '-';
-                            }
-
-                            return '<div dir="rtl" style="font-family: OpenSansHebrew-Regular; font-weight: 300; font-size: 11px;">' + date + ' - ' + title + ' : ' + value + '</div>';
-                        },
-                        positioner: function() {
-                            return { x: this.chart.chartWidth - this.label.width, y: 0 };
-                        },
-                        fixed: true
-                    };
-                    config.plotOptions = {
-                        ...config.plotOptions,
-                        series: {
-                            ...config.plotOptions.series,
-                            lineWidth: 0.5,
-                            states: {
-                                hover: {
-                                    lineWidth: 0.5
-                                }
-                            }
-                        }
-                    };
-                    config.xAxis = {
-                        ...config.xAxis,
-                        lineWidth: 0.5,
-                        lineColor: '#e8e8e8',
-                        gridLineWidth: 0.5,
-                        gridLineColor: '#e8e8e8',
-                        labels: {
-                            ...config.xAxis.labels,
-                            style: {
-                                fontSize: '11px',
-                                fontWeight: '300'
-                            }
-                        }
-                    };
-                    config.yAxis = {
-                        ...config.yAxis,
-                        gridLineWidth: 0.5,
-                        gridLineColor: '#e8e8e8',
-                        labels: {
-                            ...config.yAxis.labels,
-                            style: {
-                                fontSize: '11px',
-                                fontWeight: '300'
-                            }
-                        }
-                    };
-                    Highcharts.stockChart('container', config);
-                </script>
-            `,
             height: win.height,
             width: win.width
         };
     }
 
-    componentDidUpdate(prevProps) {
-        if (JSON.stringify(prevProps.config) !== JSON.stringify(this.props.config)) {
-            this.setState({
-                end: `
+    getChartTemplate(config) {
+        return `
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <style>
+                        @font-face {
+                            font-family: 'OpenSansHebrew-Regular';
+                            src: url('fonts/OpenSansHebrew-Regular.ttf') format('truetype');
+                            font-weight: 300;
+                        }
+                        #container {
+                            width: 100%;
+                            height: 100%;
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            font-family: 'OpenSansHebrew-Regular', sans-serif;
+                            font-weight: 300;
+                        }
+                    </style>
+                    <script>${highstockScript}</script>
+                    <script>${exportingScript}</script>
+                </head>
+                <body>
+                    <div id="container"></div>
                     <script>
-                        var config = ${JSON.stringify(this.props.config)};
+                        var config = ${JSON.stringify(config)};
                         config.chart = {
                             ...config.chart,
                             style: {
+                                fontFamily: 'OpenSansHebrew-Regular',
                                 fontSize: '11px',
                                 fontWeight: '300'
                             }
@@ -139,6 +55,7 @@ class ChartWeb extends Component {
                         config.tooltip = {
                             ...config.tooltip,
                             style: {
+                                fontFamily: 'OpenSansHebrew-Regular',
                                 fontSize: '11px',
                                 fontWeight: '300'
                             },
@@ -187,6 +104,7 @@ class ChartWeb extends Component {
                             labels: {
                                 ...config.xAxis.labels,
                                 style: {
+                                    fontFamily: 'OpenSansHebrew-Regular',
                                     fontSize: '11px',
                                     fontWeight: '300'
                                 }
@@ -199,6 +117,7 @@ class ChartWeb extends Component {
                             labels: {
                                 ...config.yAxis.labels,
                                 style: {
+                                    fontFamily: 'OpenSansHebrew-Regular',
                                     fontSize: '11px',
                                     fontWeight: '300'
                                 }
@@ -206,9 +125,9 @@ class ChartWeb extends Component {
                         };
                         Highcharts.stockChart('container', config);
                     </script>
-                `
-            });
-        }
+                </body>
+            </html>
+        `;
     }
 
     reRenderWebView = (e) => {
@@ -219,8 +138,7 @@ class ChartWeb extends Component {
     }
 
     render() {
-        const { init, end } = this.state;
-        const html = `${init}${end}`;
+        const html = this.getChartTemplate(this.props.config);
 
         return (
             <View style={this.props.style}>
